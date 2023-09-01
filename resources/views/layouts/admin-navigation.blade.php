9d2ca42307ab2ae991e26600bc829d25a5ea6e5e
kentoshiohara <a.mode.night.show@gmail.com>
2023-09-01 22:44:24 +0900

    softDelete

--- resources/views/layouts/admin-navigation.blade.php
+++ resources/views/layouts/admin-navigation.blade.php
@@ -20,6 +20,9 @@
                     <x-nav-link :href="route('admin.owners.index')" :active="request()->routeIs('admin.owners.index')">
                         オーナー管理
                     </x-nav-link>
+                    <x-nav-link :href="route('admin.expired-owners.index')" :active="request()->routeIs('admin.expiredーowners.index')">
+                        期限切れオーナー一覧
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -84,7 +87,6 @@
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('admin.logout') }}">
                     @csrf
-
                     <x-responsive-nav-link :href="route('admin.logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
